f1267b3c4dbf3e7e295ecd34081469958003187a
Tianhao Zhou <tianhaoz@umich.edu>
2018-12-19 16:05:42 -0800

    added logging library, and turned temporary cmake structure into tree like structure

--- xdev/dev/xrt/examples/hal/probe/main.cpp
+++ xdev/dev/xrt/examples/hal/probe/main.cpp
@@ -1,9 +1,17 @@
 #include "xclhal2.h"
-
+#include "easylogging++.h"
 #include <iostream>
 
 using namespace std;
 
-int main(int ac, char *av[]) {
+INITIALIZE_EASYLOGGINGPP
+
+int main(int argc, char* argv[]) {
+    START_EASYLOGGINGPP(argc, argv);
+    LOG(INFO) << "Starting Probe example ...";
+    LOG(INFO) << "Probe about to start";
+    int device_count = xclProbe();
+    LOG(INFO) << "Probe finished";
+    LOG(TRACE) << "Found " << device_count << " devices";
     return 0;
 }
