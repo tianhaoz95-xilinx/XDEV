eb99a21e3f2d0b13f923736de1792157338983a6
Tianhao Zhou <tianhaoz@umich.edu>
2018-12-21 22:03:42 -0800

    add to docs

--- docs/html/search/all_5.js
+++ docs/html/search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['main',['main',['../sysfs_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.cpp']]],
+  ['main',['main',['../probe_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp'],['../sysfs_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp']]],
   ['main_2ecpp',['main.cpp',['../probe_2main_8cpp.html',1,'']]],
   ['main_2ecpp',['main.cpp',['../sysfs_2main_8cpp.html',1,'']]]
 ];
--- xdev/dev/xrt/examples/hal/probe/main.cpp
+++ xdev/dev/xrt/examples/hal/probe/main.cpp
@@ -8,7 +8,14 @@ INITIALIZE_EASYLOGGINGPP
 
 using namespace std;
 
+/**
+ * Example for probing devices
+ */
 int main(int argc, char* argv[]) {
+    /**
+     * \b START_EASYLOGGINGPP is the entry point to easyloggingpp
+     * which only needs to be called once before any logging
+     */
     START_EASYLOGGINGPP(argc, argv);
     LOG(INFO) << "Starting Probe example ...";
     LOG(INFO) << "Probe about to start";
