f15a97cbdd5c4c76d731cc87ec018f68b890a1c3
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-02 18:01:02 -0800

    add test for debug profile device info api

--- docs/html/search/files_5.js
+++ docs/html/search/files_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['test_2ecpp',['test.cpp',['../test_8cpp.html',1,'']]]
+  ['test_2ecpp',['test.cpp',['../debug__profile__device__info_2test_8cpp.html',1,'']]],
+  ['test_2ecpp',['test.cpp',['../probe_2test_8cpp.html',1,'']]]
 ];
--- docs/html/search/functions_7.js
+++ docs/html/search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['test',['TEST',['../test_8cpp.html#a62a9e840b166d0d1f56e3c30a9fa6135',1,'test.cpp']]]
+  ['test',['TEST',['../debug__profile__device__info_2test_8cpp.html#a4438a2ad3930edfb0ddf54150b5dbe09',1,'TEST(XRT_HAL, Debug_Profile_Device_Info):&#160;test.cpp'],['../probe_2test_8cpp.html#a62a9e840b166d0d1f56e3c30a9fa6135',1,'TEST(XRT_HAL, Probe):&#160;test.cpp']]]
 ];
