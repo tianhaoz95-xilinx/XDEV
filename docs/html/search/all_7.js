c5e99bb0d299afcecbe3ba8d443af1ee86a33bf5
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-02 18:11:59 -0800

    rename meta example

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['main',['main',['../meta_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp'],['../probe_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp'],['../sysfs_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp']]],
-  ['main_2ecpp',['main.cpp',['../meta_2main_8cpp.html',1,'']]],
+  ['main',['main',['../debug__profile__device__info_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp'],['../probe_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp'],['../sysfs_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp']]],
+  ['main_2ecpp',['main.cpp',['../debug__profile__device__info_2main_8cpp.html',1,'']]],
   ['main_2ecpp',['main.cpp',['../probe_2main_8cpp.html',1,'']]],
   ['main_2ecpp',['main.cpp',['../sysfs_2main_8cpp.html',1,'']]],
   ['max_5fdebug_5fip_5flayout_5fsize',['MAX_DEBUG_IP_LAYOUT_SIZE',['../config_8hpp.html#a6180b66cb9f6a7b87262e38996479f85',1,'config.hpp']]]
--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['main',['main',['../meta_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp'],['../probe_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp'],['../sysfs_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp']]]
+  ['main',['main',['../debug__profile__device__info_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp'],['../probe_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp'],['../sysfs_2main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;main.cpp']]]
 ];
