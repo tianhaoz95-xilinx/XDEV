108484191789cdf005204d6edab6044c752cc8e2
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-04 18:58:40 -0800

    update developer doc

--- docs/developer/html/search/all_d.js
+++ docs/developer/html/search/all_d.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['read_5fsysfs_5fhelper_2ehpp',['read_sysfs_helper.hpp',['../read__sysfs__helper_8hpp.html',1,'']]],
-  ['read_5fsysfs_5fwith_5fconfig',['read_sysfs_with_config',['../read__sysfs__helper_8hpp.html#a5fd6e363beb90e1b0b7f2376fb8ef519',1,'read_sysfs_helper.hpp']]]
+  ['read_5fsysfs_5fwith_5fconfig',['read_sysfs_with_config',['../read__sysfs__helper_8hpp.html#a3a2b8407eb8a504ce33c1b08363262db',1,'read_sysfs_helper.hpp']]]
 ];
--- docs/developer/html/search/functions_7.js
+++ docs/developer/html/search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['read_5fsysfs_5fwith_5fconfig',['read_sysfs_with_config',['../read__sysfs__helper_8hpp.html#a5fd6e363beb90e1b0b7f2376fb8ef519',1,'read_sysfs_helper.hpp']]]
+  ['read_5fsysfs_5fwith_5fconfig',['read_sysfs_with_config',['../read__sysfs__helper_8hpp.html#a3a2b8407eb8a504ce33c1b08363262db',1,'read_sysfs_helper.hpp']]]
 ];
