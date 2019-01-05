5c15210e22c3d00e47bd014e6b6d45d465c164a6
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-04 19:13:22 -0800

    add demo docs

--- docs/demo/site/xrt/examples/hal/sysfs/sysfs_demo.cpp
+++ docs/demo/site/xrt/examples/hal/sysfs/sysfs_demo.cpp
@@ -47,30 +47,13 @@ int sysfs_demo(int argc, char* argv[]) {
     char data[MAX_DEBUG_IP_LAYOUT_SIZE];
     LOG(INFO) << "Querying device[0] sysfs ...";
     LOG(INFO) << "Querying device[0] sysfs with fixed size ...";
-    read_sysfs_with_config(device_handle, false, "", "debug_ip_layout", MAX_DEBUG_IP_LAYOUT_SIZE, (void*)&data[0]);
+    read_sysfs_with_config(device_handle, "icap", "debug_ip_layout", MAX_DEBUG_IP_LAYOUT_SIZE, (void*)&data[0]);
     LOG(INFO) << "Visualizing query result for Debug IP Layout ...";
     visualize_debug_ip_layout((void*)&data[0]);
     LOG(INFO) << "Querying device[0] sysfs with auto size detection ...";
-    read_sysfs_with_config(device_handle, true, "", "debug_ip_layout", 0, (void*)&data[0]);
+    read_sysfs_with_config(device_handle, "icap", "debug_ip_layout", 0, (void*)&data[0]);
     LOG(INFO) << "Visualizing query result for Debug IP Layout ...";
     visualize_debug_ip_layout((void*)&data[0]);
-    LOG(INFO) << "Querying device[0] sysfs with auto size detection and size as non-zero (should throw an error) ...";
-    bool expected_exception_caught = false;
-    try {
-        read_sysfs_with_config(device_handle, true, "", "debug_ip_layout", 10, (void*)&data[0]);
-    } catch (const runtime_error& e) {
-        string expected_error_msg = "Read sysfs failed";
-        if (string(e.what()) == expected_error_msg) {
-            LOG(INFO) << "Expected failure with error message: " << e.what();
-            expected_exception_caught = true;
-        } else {
-            LOG(INFO) << "Unexpected failure with error message: " << e.what() << ", expecting " << expected_error_msg;
-        }
-    }
-    if (!expected_exception_caught) {
-        LOG(INFO) << "Query with read_all and size as non-zero should throw an exception, but non seen";
-        throw runtime_error("Expected exception not seen");
-    }
     LOG(INFO) << "Closing device[0] ...";
     xclClose(device_handle);
     LOG(INFO) << "device[0] closed";
