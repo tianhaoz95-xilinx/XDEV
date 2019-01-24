ce82016b85bef9060025b7722f79d0323db154b4
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-24 11:09:26 -0800

    check if device is found

--- xdev/dev/xrt/driver/nifd/nifd_alternate_xclbin.cpp
+++ xdev/dev/xrt/driver/nifd/nifd_alternate_xclbin.cpp
@@ -14,6 +14,10 @@ int nifd_alternate_xclbin(int argc, char* argv[]) {
         return 0;
     }
     auto devices = retrieve_device_by_name(xilinx_platforms[0], "xilinx_u200_xdma_201830_2");
+    if (devices.empty()) {
+        LOG(WARNING) << "No valid device found";
+        return 0;
+    }
     auto device = devices[0];
     auto context = cl::Context(device);
     cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE);
