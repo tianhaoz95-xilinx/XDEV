f0bbe334750d1305db04c576cd85c1c95d49ea29
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-24 13:54:27 -0800

    scope limit the opencl objects

--- xdev/dev/xrt/driver/nifd/nifd_alternate_xclbin.cpp
+++ xdev/dev/xrt/driver/nifd/nifd_alternate_xclbin.cpp
@@ -7,7 +7,27 @@
 
 INITIALIZE_EASYLOGGINGPP
 
-int nifd_alternate_xclbin(int argc, char* argv[]) {
+void load_hello_xclbin() {
+    auto xilinx_platforms = retrieve_platform_by_name("Xilinx");
+    if (xilinx_platforms.empty()) {
+        LOG(WARNING) << "No Xilinx platform found";
+        return 0;
+    }
+    auto devices = retrieve_device_by_name(xilinx_platforms[0], "xilinx_u200_xdma_201830_2");
+    if (devices.empty()) {
+        LOG(WARNING) << "No valid device found";
+        return 0;
+    }
+    auto device = devices[0];
+    auto context = cl::Context(device);
+    cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE);
+    string hello_filename = "/home/xsjbrd6/Desktop/darkside/farm/alveo_u200_nifd_experimental/hello/hello_kernel_hw_all.xclbin";
+    LOG(INFO) << "Loading XCLBIN from " << hello_filename << " ...";
+    cl::Program hello_program = load_xclbin_create_program(context, {device}, hello_filename);
+    LOG(INFO) << "XCLBIN from " << hello_filename << " loaded";
+}
+
+void load_vadd_xclbin() {
     auto xilinx_platforms = retrieve_platform_by_name("Xilinx");
     if (xilinx_platforms.empty()) {
         LOG(WARNING) << "No Xilinx platform found";
@@ -22,16 +42,19 @@ int nifd_alternate_xclbin(int argc, char* argv[]) {
     auto context = cl::Context(device);
     cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE);
     string vadd_filename = "/home/xsjbrd6/Desktop/darkside/farm/alveo_u200_nifd_experimental/vadd/vadd_kernel_hw_all.xclbin";
-    string hello_filename = "/home/xsjbrd6/Desktop/darkside/farm/alveo_u200_nifd_experimental/hello/hello_kernel_hw_all.xclbin";
-    LOG(INFO) << "Loading XCLBIN from " << hello_filename << " ...";
-    cl::Program hello_program = load_xclbin_create_program(context, {device}, hello_filename);
-    LOG(INFO) << "XCLBIN from " << hello_filename << " loaded";
-    // read back from NIFD
-    device.release();
     LOG(INFO) << "Loading XCLBIN from " << vadd_filename << " ...";
     cl::Program vadd_program = load_xclbin_create_program(context, {device}, vadd_filename);
     LOG(INFO) << "XCLBIN from " << vadd_filename << " loaded";
-    // check for card hang
+}
+
+void nifd_operation() {
+    return;
+}
+
+int nifd_alternate_xclbin(int argc, char* argv[]) {
+    load_hello_xclbin();
+    nifd_operation();
+    load_vadd_xclbin();
     return 0;
 }
 
