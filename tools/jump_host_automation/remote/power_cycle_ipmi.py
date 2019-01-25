f54e5e4ff5b4f184825b7f936868818ddede46b1
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-25 15:59:42 -0800

    add remote for testing

--- tools/jump_host_automation/remote/power_cycle_ipmi.py
+++ tools/jump_host_automation/remote/power_cycle_ipmi.py
@@ -4,6 +4,8 @@ import pyipmi.interfaces
 def main():
     interface = pyipmi.interfaces.create_interface('ipmitool', interface_type='lan')
     connection = pyipmi.create_connection(interface)
+    connection.target = pyipmi.Target(0x82)
+    connection.target.set_routing([(0x81,0x20,0),(0x20,0x82,7)])
     connection.session.set_session_type_rmcp('10.0.0.1')
     connection.session.set_auth_type_user('ADMIN', 'ADMIN')
     connection.session.establish()
