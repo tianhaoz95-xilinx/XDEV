37a245fba0bff04996368aba11abbbe7ecf66f10
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-25 16:08:03 -0800

    add remote for testing

--- tools/jump_host_automation/remote/power_cycle_ipmi.py
+++ tools/jump_host_automation/remote/power_cycle_ipmi.py
@@ -2,6 +2,8 @@ import paramiko
 
 def main():
     client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect('10.23.53.114', username='ADMIN', password='ADMIN')
     stdin, stdout, stderr = client.exec_command('show')
     for line in stdout:
