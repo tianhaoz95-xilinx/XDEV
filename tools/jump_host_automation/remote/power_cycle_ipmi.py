7a98f156c3127159beb0be81cfe3ff555fa7f45e
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-25 16:11:50 -0800

    add remote for testing

--- tools/jump_host_automation/remote/power_cycle_ipmi.py
+++ tools/jump_host_automation/remote/power_cycle_ipmi.py
@@ -1,14 +1,15 @@
-import paramiko
+import pyipmi
+import pyipmi.interfaces
 
 def main():
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect('10.23.53.114', username='ADMIN', password='ADMIN')
-    stdin, stdout, stderr = client.exec_command('show')
-    for line in stdout:
-        print('... ' + line.strip('\n'))
-    client.close()
+    interface = pyipmi.interfaces.create_interface('ipmitool', interface_type='lan')
+    connection = pyipmi.create_connection(interface)
+    connection.target = pyipmi.Target(0x82)
+    connection.target.set_routing([(0x81,0x20,0),(0x20,0x82,7)])
+    connection.session.set_session_type_rmcp('10.0.0.1')
+    connection.session.set_auth_type_user('ADMIN', 'ADMIN')
+    connection.session.establish()
+    print(connection.get_device_id())
 
 if __name__ == '__main__':
     main()
