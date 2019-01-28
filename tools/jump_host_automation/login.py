f1188b531a1f24c01268e6f8c11f49613dd962a5
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-27 22:28:18 -0800

    work in progress

--- tools/jump_host_automation/login.py
+++ tools/jump_host_automation/login.py
@@ -1,6 +1,6 @@
 from user_input import get_user_information
 
-def login_workstation(config, workstation_type, index, lab):
+def login_workstation(config, workstation_type, id, lab):
     workstation_hostname = get_user_information('Enter the hostname of the ' + index + ' jump host: ', config['hostname'])
     workstation_username = get_user_information('Enter the username of ' + workstation_hostname + ': ', config['username'])
     workstation_password = get_user_information('Enter the password of ' + workstation_username + '@' + workstation_hostname + ': ' , config['password'])
