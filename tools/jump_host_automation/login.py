02547df0ef37f4627414e8d24553bde2a7341383
Tianhao Zhou <tianhaoz@umich.edu>
2019-01-28 13:00:50 -0800

    debug hal writing

--- tools/jump_host_automation/login.py
+++ tools/jump_host_automation/login.py
@@ -9,7 +9,7 @@ def login_workstation(config, workstation_type, workstation_id, lab):
         workstation_title = 'jump host'
     if workstation_type == 'machine':
         workstation_title = 'lab machine'
-    workstation_hostname = get_user_information('Enter the hostname of the ' + index + ' ' + workstation_id + ': ', config['hostname'])
+    workstation_hostname = get_user_information('Enter the hostname of the ' + workstation_title + ' ' + workstation_id + ': ', config['hostname'])
     workstation_username = get_user_information('Enter the username of ' + workstation_hostname + ': ', config['username'])
     workstation_password = get_user_information('Enter the password of ' + workstation_username + '@' + workstation_hostname + ': ' , config['password'])
     if workstation_type == 'jump_host':
--- xdev/dev/xrt/driver/nifd/nifd_alternate_xclbin.cpp
+++ xdev/dev/xrt/driver/nifd/nifd_alternate_xclbin.cpp
@@ -92,7 +92,7 @@ void reset_icap_with_hal() {
     LOG(INFO) << "Writing value 0x" << std::hex << reg_target_value << " to ICAP address 0x" << icap_offset << std::dec << " ...";
     int err = xclUnmgdPwrite(device_handle, 0, (void*)(&reg_target_value), sizeof(unsigned int), icap_offset);
     LOG(INFO) << "Writing to ICAP finished with return code: " << err;
-    if (err) {
+    if (err < 0) {
         LOG(ERROR) << "Unmanaged write to ICAP reset failed";
         throw runtime_error("reset ICAP failed");
     }
