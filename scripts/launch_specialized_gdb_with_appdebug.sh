efa137eb7165639cefaf67c8f523d12fc2d782a9
Tianhao Zhou <tianhaoz@umich.edu>
2019-02-07 10:28:28 -0800

    add python home

--- scripts/launch_specialized_gdb_with_appdebug.sh
+++ scripts/launch_specialized_gdb_with_appdebug.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+export PYTHONPATH=$PYTHONHOME/lib/python2.7/lib-dynload/:$PYTHONHOME/lib/python2.7/
 exec "$SPECIAL_GDB_PATH/bin/gdb" -ex "source $XILINX_XRT/share/appdebug/appdebug.py" "${@:1}"
