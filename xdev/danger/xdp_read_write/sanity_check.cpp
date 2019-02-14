4b0b669a8bac478676f0162221b94aaefe0ab89c
Tianhao Zhou <tianhaoz@umich.edu>
2019-02-14 11:51:48 -0800

    quick update

--- xdev/danger/xdp_read_write/sanity_check.cpp
+++ xdev/danger/xdp_read_write/sanity_check.cpp
@@ -6,7 +6,7 @@ int sanity_check(int argc, char* argv[]) {
     int device_count = xclProbe();
     int device_index = 0;
     xclDeviceHandle device_handle = xclOpen(device_index, "open_devices_demo.log", xclVerbosityLevel::XCL_INFO);
-    auto profile_ip = xdp::ProfileIP(device_handle, 0);
+    auto profile_ip = xdp::ProfileIP(device_handle, 6);
     profile_ip.map();
     unsigned data[256];
     data[0] = 15;
